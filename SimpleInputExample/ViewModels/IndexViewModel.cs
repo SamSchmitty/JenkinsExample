c4da807bdbec1e6c91a95e208ee2b89ec38f594c
Samuel Schmitt <sam.schmitty@gmail.com>
2024-01-07 17:46:48 -0600

    adding summary and testing webhook

--- SimpleInputExample/ViewModels/IndexViewModel.cs
+++ SimpleInputExample/ViewModels/IndexViewModel.cs
@@ -4,6 +4,13 @@ namespace SimpleInputExample.ViewModels;
 
 public class IndexViewModel : PageModel
 {
+    /// <summary>
+    /// Input for the page
+    /// </summary>
     public int DivisibleByTwoInput { get; set; } = 0;
+
+    /// <summary>
+    /// Validation message sent to display on the page
+    /// </summary>
     public string Message { get; set; } = "";
 }
